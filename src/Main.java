d4bf3a435598b71dddbdb7ac1b8ef05d8076bf7c
emiiv <emilio.lopez@gmail.com>
2023-11-13 15:08:04 -0600

    added jFrame

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Ventana");
+        JPanel jPanel1 = new JPanel();
+        JLabel jLabel1 = new JLabel("Ejemplo");
+        jPanel1.add(jLabel1);
+        jFrame.add(jPanel1);
+        jFrame.setSize(400, 400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
 }
