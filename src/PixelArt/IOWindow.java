7dd686253a89dce5aa66eba4bddab343e41d2fde
emiiv <emilio.lopez@gmail.com>
2023-11-21 17:20:11 -0600

    general optimisations

--- src/PixelArt/IOWindow.java
+++ src/PixelArt/IOWindow.java
@@ -1,15 +1,59 @@
 package PixelArt;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class IOWindow extends JFrame {
-    IOPanel ioPanel;
+public class IOWindow extends JFrame implements ActionListener {
+    JPanel ioPanel;
+    JButton newFileBtn, searcFileBtn;
+    JLabel textLabel;
+    GridBagConstraints gridBagConstraints = new GridBagConstraints();
     public IOWindow(){
         setName("PixArt: Archivos");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1920, 1080);
         setExtendedState(MAXIMIZED_BOTH);
-        add(ioPanel = new IOPanel());
+        add(ioPanel = generateIOPanel());
         setVisible(true);
+    }
+    public JPanel generateIOPanel(){
+        JPanel IOPanel = new JPanel();
+        IOPanel.setLayout(new GridBagLayout());
+        gridBagConstraints.insets = new Insets(100,100,100,100);
+        textLabel = new JLabel("Desea abrir una imagen para editar o crear un nuevo archivo?");
+        textLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 1;
+        gridBagConstraints.weighty = 1;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        IOPanel.add(textLabel, gridBagConstraints);
+        newFileBtn = new JButton("Nuevo Archivo");
+        newFileBtn.addActionListener(this);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.weightx = 1;
+        gridBagConstraints.weighty = 1;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        IOPanel.add(newFileBtn, gridBagConstraints);
+        searcFileBtn = new JButton("Buscar Archivo");
+        searcFileBtn.addActionListener(this);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.weightx = 1;
+        gridBagConstraints.weighty = 1;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        IOPanel.add(searcFileBtn, gridBagConstraints);
+        return IOPanel;
+    }
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource().equals(newFileBtn)) {
+            EditorPanel editorPanel = new EditorPanel(1080, 720, 10);
+            EditorWindow editorWindow = new EditorWindow(editorPanel);
+            setVisible(false);
+        }
     }
 }
--- src/PixelArt/MenuWindow.java
+++ src/PixelArt/MenuWindow.java
@@ -1,17 +1,61 @@
 package PixelArt;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MenuWindow extends JFrame {
+public class MenuWindow extends JFrame implements ActionListener{
     JPanel menuPanel;
+    JButton startBtn, exitBtn;
+    JLabel titleLbl;
+    GridBagConstraints gridBagConstraints = new GridBagConstraints();
     public MenuWindow(){
         setTitle("PixArt: Menu");
         setSize(1280, 720);
         setExtendedState(MAXIMIZED_BOTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        add(menuPanel = new MainMenuPanel());
+        add(menuPanel = createMenuPanel());
         setVisible(true);
+    }
+    public JPanel createMenuPanel(){
+        JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new GridBagLayout());
+        gridBagConstraints.insets = new Insets(100,100,100,100);
+        titleLbl = new JLabel("PixArt");
+        titleLbl.setHorizontalAlignment(SwingConstants.CENTER);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 1;
+        gridBagConstraints.weighty = 1;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        menuPanel.add(titleLbl, gridBagConstraints);
+        startBtn = new JButton("Iniciar");
+        startBtn.addActionListener(this);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        menuPanel.add(startBtn, gridBagConstraints);
+        exitBtn = new JButton("Salir");
+        exitBtn.addActionListener(this);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        menuPanel.add(exitBtn, gridBagConstraints);
+        return menuPanel;
+    }
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        System.out.println(event);
+        if (event.getSource().equals(exitBtn)) {
+            System.out.println("Boton SALIR presionado");
+            JOptionPane.showMessageDialog(null, "Gracias por usar PixArt.\n" +
+                    "Saliendo...", "Exit", JOptionPane.INFORMATION_MESSAGE);
+            System.out.println("TERMINANDO PROGRAMA...");
+            System.exit(0);
+        } else if (event.getSource().equals(startBtn)) {
+            IOWindow ioWindow = new IOWindow();
+            this.setVisible(false);
+        }
     }
 }
