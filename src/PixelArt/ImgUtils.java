6d2a6f7d5d8794d3ee8303a4242b1214bc7ce4c5
emiiv <emilio.lopez@gmail.com>
2023-11-23 14:56:17 -0600

    image IO bugs pending fix

--- src/PixelArt/ImgUtils.java
+++ src/PixelArt/ImgUtils.java
@@ -7,7 +7,6 @@ public class ImgUtils {
     public static BufferedImage resize(BufferedImage img, int newW, int newH) {
         Image tmp = img.getScaledInstance(newW, newH, img.SCALE_SMOOTH);
         BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-
         Graphics2D g2d = dimg.createGraphics();
         g2d.drawImage(tmp, 0, 0, null);
         g2d.dispose();
